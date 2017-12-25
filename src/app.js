54853366d08224c3186ca7dc0f6a6a092f8f9bf3
tiagocorreia <tiagoalmeida.correia@gmail.com>
2017-12-25 22:59:13 +0000

    CSS scripts/file for production

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import GameApp from "./components/GameApp";
-import "normalize.css/normalize.css";
-import "./styles/main.scss";
 
 ReactDOM.render(<GameApp />, document.getElementById("app"));
