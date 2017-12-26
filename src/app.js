60e63fcc8476360676105e86e9be25e3aafecb2f
Tiago Correia <tiagoalmeida.correia@gmail.com>
2017-12-26 10:26:46 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import GameApp from "./components/GameApp";
+import "normalize.css/normalize.css";
+import "./styles/main.scss";
 
 ReactDOM.render(<GameApp />, document.getElementById("app"));
